2d4a1f4656bdef28a7ef6537d5eee028e0d9253f
chrzyki <christoph@functor-argument.net>
2023-12-13 14:23:59 +0100

    Add 00_setup comments

--- scripts/00_setup.R
+++ scripts/00_setup.R
@@ -1,6 +1,8 @@
 install.packages("groundhog", repos="https://ftp.fau.de/cran/")
 library("groundhog")
+# TODO: setwd() for this R session to scripts/? Or ask users to setwd() accordingly, otherwise groundhog & paths don't work as expected.
 set.groundhog.folder("./R_groundhog/")
+# TODO: cmdstanr is required as well, if not for the priors then for the models. Mention either here or in 03/04/05?
 pkgs <- c("brms","viridis", "readr", "posterior", "dplyr", "ggplot2", "ggdist",
           "gghalves", "patchwork", "bayesplot", "tidybayes", "xtable",
           "ggrepel", "rnaturalearth", "rnaturalearthdata", "tidyr", "stringr")
