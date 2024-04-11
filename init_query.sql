dd0422dcb86f9f410c8681209930d6f452a9a42d
Frederic Blum <frederic_blum@eva.mpg.de>
2024-04-11 11:09:47 +0200

    add non-pulmonic

--- init_query.sql
+++ init_query.sql
@@ -10,15 +10,6 @@ SELECT
     CASE
         WHEN phone.cldf_id in (select cldf_id FROM word_initials) THEN 1 ELSE 0
         END word_initial, -- whether or not the phone is in word initial position
-	CASE
-		WHEN sound.cldf_cltsReference LIKE '%devoiced%' THEN 'voiceless' 
-		WHEN sound.cldf_cltsReference LIKE '%voiceless%' THEN 'voiceless' ELSE 'voiced'
-		END voicing,
-    CASE
-        WHEN sound.cldf_cltsReference LIKE '%stop%' THEN 'stop'
-		WHEN sound.cldf_cltsReference LIKE '%affricate%' THEN 'stop'
-		WHEN sound.cldf_cltsReference LIKE '%fricative%' THEN 'fricative' ELSE 'sonorant'
-        END sound_class,
     -- normalized word length:
 	ROUND(((phones_per_word.num_phones - sd_num_phones.avg_num_phones) / sd_num_phones.num_phones), 3) AS z_num_phones,
 	-- normalized speech rate of the utterance:
@@ -128,9 +119,6 @@ WHERE
     phone.cldf_parameterReference = sound.cldf_id AND
     -- We only consider non-long, pulmonic consonants ...
     sound.cldf_cltsReference LIKE '%_consonant' AND
-    sound.cldf_cltsReference NOT LIKE '%click%' AND
-    sound.cldf_cltsReference NOT LIKE '%implosive%' AND
-    sound.cldf_cltsReference NOT LIKE '%ejective%' AND
     sound.cldf_cltsReference NOT LIKE '%long%' AND
     -- ... and exclude utterance-initial stops.
     NOT (phone.cldf_id in (select cldf_id from utterance_initials) AND sound.cldf_cltsReference LIKE '%stop%') AND
