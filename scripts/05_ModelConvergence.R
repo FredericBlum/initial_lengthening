89d8619fa99b7753fbf7b3d866010f6340643946
FredericBlum <frederic_blum@eva.mpg.de>
2024-04-25 09:52:18 +0200

    run predict

--- scripts/05_ModelConvergence.R
+++ scripts/05_ModelConvergence.R
@@ -22,83 +22,84 @@ data <- read_tsv('data.tsv') %>%
 mutate(cluster = as.factor(ifelse(cluster_status=='clusterInitial', "initial", ifelse(
 	      cluster_status=='noCluster', 'single', 'nonInitial'))),
        word_initial=as.factor(word_initial),
-       utt_initial=as.factor(word_initial))
+       utt_initial=as.factor(word_initial),
+       IPA=CLTS)
 
 model <- readRDS(file="models/cl_Speaker.rds")
-
-#########################################
-###     model convergence             ###
-#########################################
-lp_max <- log_posterior(model)
-np_max <- nuts_params(model)
-posterior_max <- as.array(model)
-
-# mcmc_pairs: up to 8 parameters, divergent transitions and collinearity between parameters
-pred_pairs <- mcmc_pairs(posterior_max, np=np_max,
-                         pars=c(
-                           'b_Intercept', 'b_z_word_freq', 'b_z_num_phones',
-                           'b_word_initial1', 'b_utt_initial1', 'shape'
-                           ), 
-                         off_diag_args=list(size=0.75))
-
-diver_scatter <- mcmc_scatter(posterior_max, np=np_max, pars=c('b_Intercept', 'shape'))
-ggsave('images/viz_pairsPred.png', pred_pairs, scale=1.3,
-       width=3000, height=2800, units="px")
-
-zipfs_pairs <- mcmc_pairs(posterior_max, np=np_max, regex_pars=c("^b_z_"), 
-                         off_diag_args=list(size=0.75))
-
-ggsave('images/viz_pairsPredz.png', zipfs_pairs, scale=1.3,
-       width=3000, height=2800, units="px")
-
-collinearity_pred <- as_draws_df(model) %>% 
-  select(b_z_num_phones:b_z_word_freq) %>% 
-  cor()
-
-print(paste(
-  "The correlation between word form frequenzy and number of phones per word is",
-  round(collinearity_pred[1,2], 2)
-  ))
-
-# trank plots for intercept and sigma
-both_traces <- mcmc_trace(posterior_max, pars=c("b_Intercept", "shape"), 
-                          facet_args=list(ncol=2, strip.position="left")) +
-  theme(legend.position="none")
-
-both_ranks <- mcmc_rank_overlay(posterior_max, pars=c("b_Intercept", "shape"), 
-                                facet_args=list(ncol=2, strip.position="left")) +
-  # coord_cartesian(ylim=c(0, 200)) + 
-  theme(legend.position="none")
-
-eval_chains <- (both_traces /  both_ranks)
-
-ggsave('images/eval_chains.png', eval_chains, scale=1)
-
-########################################
-rhats_max <- brms::rhat(model)
-rhat_all <- mcmc_rhat(rhats_max) + xlab("R-hat value") +
-  scale_x_continuous(breaks=c(1.0, 1.005, 1.01), limits=c(1, 1.01)) +
-  theme(legend.position="none")
-
-rhat_val <- rhats_max[lapply(rhats_max, as.numeric) > 1.004]
-rhat_filter <- mcmc_rhat(rhat_val) + yaxis_text(hjust=1) +
-  scale_x_continuous(breaks=c(1, 1.005, 1.01), limits=c(1, 1.015)) +
-  theme(legend.position="none")
-
-###########################################
-neff_max <- neff_ratio(model)
-neff_all_plot <- mcmc_neff(neff_max, size=2) + 
-  scale_x_continuous(breaks=c(0, 0.5, 1)) +
-  xlab("Effective sample size")  +
-  theme(legend.position="none")
-
-neff_filter <- neff_max[lapply(neff_max, as.numeric) < 0.15]
-neff_fil_plot <- mcmc_neff(neff_filter, size=2) + yaxis_text(hjust=1) + 
-  scale_x_continuous(breaks=c(0.1, 0.2), limits=c(0, 0.2))  +
-  theme(legend.position="none")
-
-rhat_neff_plot <- (rhat_all + rhat_filter) / (neff_all_plot + neff_fil_plot)
-ggsave('images/eval_rhat_neff.png', rhat_neff_plot, scale=1)
+# 
+# #########################################
+# ###     model convergence             ###
+# #########################################
+# lp_max <- log_posterior(model)
+# np_max <- nuts_params(model)
+# posterior_max <- as.array(model)
+# 
+# # mcmc_pairs: up to 8 parameters, divergent transitions and collinearity between parameters
+# pred_pairs <- mcmc_pairs(posterior_max, np=np_max,
+#                          pars=c(
+#                            'b_Intercept', 'b_z_word_freq', 'b_z_num_phones',
+#                            'b_word_initial1', 'b_utt_initial1', 'shape'
+#                            ), 
+#                          off_diag_args=list(size=0.75))
+# 
+# diver_scatter <- mcmc_scatter(posterior_max, np=np_max, pars=c('b_Intercept', 'shape'))
+# ggsave('images/viz_pairsPred.png', pred_pairs, scale=1.3,
+#        width=3000, height=2800, units="px")
+# 
+# zipfs_pairs <- mcmc_pairs(posterior_max, np=np_max, regex_pars=c("^b_z_"), 
+#                          off_diag_args=list(size=0.75))
+# 
+# ggsave('images/viz_pairsPredz.png', zipfs_pairs, scale=1.3,
+#        width=3000, height=2800, units="px")
+# 
+# collinearity_pred <- as_draws_df(model) %>% 
+#   select(b_z_num_phones:b_z_word_freq) %>% 
+#   cor()
+# 
+# print(paste(
+#   "The correlation between word form frequenzy and number of phones per word is",
+#   round(collinearity_pred[1,2], 2)
+#   ))
+# 
+# # trank plots for intercept and sigma
+# both_traces <- mcmc_trace(posterior_max, pars=c("b_Intercept", "shape"), 
+#                           facet_args=list(ncol=2, strip.position="left")) +
+#   theme(legend.position="none")
+# 
+# both_ranks <- mcmc_rank_overlay(posterior_max, pars=c("b_Intercept", "shape"), 
+#                                 facet_args=list(ncol=2, strip.position="left")) +
+#   # coord_cartesian(ylim=c(0, 200)) + 
+#   theme(legend.position="none")
+# 
+# eval_chains <- (both_traces /  both_ranks)
+# 
+# ggsave('images/eval_chains.png', eval_chains, scale=1)
+# 
+# ########################################
+# rhats_max <- brms::rhat(model)
+# rhat_all <- mcmc_rhat(rhats_max) + xlab("R-hat value") +
+#   scale_x_continuous(breaks=c(1.0, 1.005, 1.01), limits=c(1, 1.01)) +
+#   theme(legend.position="none")
+# 
+# rhat_val <- rhats_max[lapply(rhats_max, as.numeric) > 1.004]
+# rhat_filter <- mcmc_rhat(rhat_val) + yaxis_text(hjust=1) +
+#   scale_x_continuous(breaks=c(1, 1.005, 1.01), limits=c(1, 1.015)) +
+#   theme(legend.position="none")
+# 
+# ###########################################
+# neff_max <- neff_ratio(model)
+# neff_all_plot <- mcmc_neff(neff_max, size=2) + 
+#   scale_x_continuous(breaks=c(0, 0.5, 1)) +
+#   xlab("Effective sample size")  +
+#   theme(legend.position="none")
+# 
+# neff_filter <- neff_max[lapply(neff_max, as.numeric) < 0.15]
+# neff_fil_plot <- mcmc_neff(neff_filter, size=2) + yaxis_text(hjust=1) + 
+#   scale_x_continuous(breaks=c(0.1, 0.2), limits=c(0, 0.2))  +
+#   theme(legend.position="none")
+# 
+# rhat_neff_plot <- (rhat_all + rhat_filter) / (neff_all_plot + neff_fil_plot)
+# ggsave('images/eval_rhat_neff.png', rhat_neff_plot, scale=1)
 
 #########################################
 ###     posterior predictive checks   ###
