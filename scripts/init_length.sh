9c5936e69c0be0166ae9a2fa0fd70eea048e31dd
FredericBlum <frederic_blum@eva.mpg.de>
2024-04-29 07:53:23 +0200

    rm err out

--- scripts/init_length.sh
+++ scripts/init_length.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 #SBATCH --cpus-per-task 2
-#SBATCH --mem-per-cpu 350G
+#SBATCH --mem-per-cpu 240G
 #SBATCH -J init_length
 #SBATCH --output=%x.%j.out
 #SBATCH --error=%x.%j.err
-#SBATCH -w dlcenode14
+#SBATCH --partition dlcegpu
+#SBATCH -w dlcenode01
 
 Rscript --verbose 05_ModelConvergence.R
